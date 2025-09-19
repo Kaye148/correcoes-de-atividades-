87b8bb55e3a2185466aea387c7fb329fa6e4ff52
Kayky Sampaio <kaykysampaio13@gmail.com>
2025-09-19 16:47:44 -0300

    Add files via upload

---
+++ 9atividade_calculo.py
@@ -0,0 +1,21 @@
+import os
+os.system
+
+pares = 0
+impares = 0
+soma = 0
+quantidade_numeros = 0
+media_geral = 0
+
+while True:
+    numero = int(input("digite um número: "))
+    if pares %2 == 0:
+        break
+    else:
+        impares %2 == 0
+    quantidade_numeros +=1
+    media = pares + quantidade_numeros/2
+
+    print(f"Média:{media}")
+
+
